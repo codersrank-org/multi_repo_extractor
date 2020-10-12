60143ac89688eec2949a1a90cd135c52ad5fa3b6
Alim Giray Aytar <alimgiray@gmail.com>
2020-10-12 12:33:39 +0300

    fix typo

--- repo/entity/repository.go
+++ repo/entity/repository.go
@@ -1,6 +1,6 @@
 package entity
 
-// Repository is the internal representation of external repository informations
+// Repository is the internal representation of external repository information
 type Repository struct {
 	ID       int
 	FullName string
